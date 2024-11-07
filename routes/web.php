937279230accf8cdcb7b4eb1df8ed96b0479c4cc
Daryl Chymko <daryl@getoutside.ca>
2024-11-07 07:40:34 -0800

    Final tweaks to get site working

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+Route::statamic('posts/type/{type}', 'type')->name('posts.type');
+Route::statamic('sitemap.xml', 'sitemap', ['content_type' => 'text/xml'])->name('sitemap');
+Route::statamic('feed.xml', 'feed', ['content_type' => 'text/xml'])->name('feed');
+Route::statamic('search', 'search_results')->name('search');
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
 // ]);
